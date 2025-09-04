65839e97dbfbf14f289a464e77503a2b1232bdcd
Manzai-1 <0xdecentmaz@gmail.com>
2025-09-04 14:02:14 +0200

    Add foodTypes and feed function

--- contracts/Tamagochi.sol
+++ contracts/Tamagochi.sol
@@ -2,36 +2,61 @@
 pragma solidity 0.8.28;
 
 contract Tamagochi{
+
     address private owner; 
 
- struct Meals {
-    string name;
-    uint16 points;
- }
- struct TamaCreature {
-    string name;
-    uint16 foodLvl;
-    uint16 happinessLvl;
- }
- 
- constructor() {
-    owner = msg.sender;
+    struct Meal {
+        string name;
+        uint16 points;
+        bool exists;
+    }
 
- }
+    struct TamaCreature {
+        string name;
+        uint16 foodLvl;
+        uint16 happinessLvl;
+    }
 
- mapping (address => TamaCreature) creatures;
+    mapping(string => Meal) public meals;
 
- function addTama( string memory name) public{
-    creatures[msg.sender] = (TamaCreature({
-        name: name,
-        foodLvl: 50,
-        happinessLvl: 50
-    }));
- }
+    constructor() {
+        owner = msg.sender;
 
- function getMyCreature() public  view returns (string memory name){
-    name = creatures[msg.sender].name;
+        meals["Kibble"] = (Meal({
+            name: "Kibble",
+            points: 5,
+            exists: true
+        }));
 
- }
+        meals["Churro"] = (Meal({
+            name: "Churro",
+            points: 10,
+            exists: true
+        }));
+
+        meals["Steak"] = (Meal({
+            name: "Steak",
+            points: 15,
+            exists: true
+        }));
+    }
+
+    mapping (address => TamaCreature) creatures;
+
+    function addTama( string memory name) public{
+        creatures[msg.sender] = (TamaCreature({
+            name: name,
+            foodLvl: 50,
+            happinessLvl: 50
+        }));
+    }
+
+    function getMyCreature() public  view returns (string memory name){
+        name = creatures[msg.sender].name;
+    }
+
+    function feedMyCreature(string memory meal) public {
+        creatures[msg.sender].foodLvl += meals[meal].points;
+    }
 
 }
