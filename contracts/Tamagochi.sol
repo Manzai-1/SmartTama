79779790d2797799f52794f0653e0e9926857583
Manzai-1 <0xdecentmaz@gmail.com>
2025-09-04 11:33:07 +0200

    Add initial hardhat project with base contract containing datatypes and create / read tama functions

---
+++ contracts/Tamagochi.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+contract Tamagochi{
+    address private owner; 
+
+ struct Meals {
+    string name;
+    uint16 points;
+ }
+ struct TamaCreature {
+    string name;
+    uint16 foodLvl;
+    uint16 happinessLvl;
+ }
+ 
+ constructor(address customOwner) {
+    owner = customOwner; 
+
+ }
+
+ mapping (address => TamaCreature) creatures;
+
+ function addTama( string memory name) public{
+    creatures[msg.sender] = (TamaCreature({
+        name: name,
+        foodLvl: 50,
+        happinessLvl: 50
+    }));
+ }
+
+ function getMyCreature() public  view returns (string memory name){
+    name = creatures[msg.sender].name;
+
+ }
+
+}
